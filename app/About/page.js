46ebd3076d0064d498c197745b3d4d76844912d6
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-09-01 01:48:20 +0200

    fixing deployment issue

--- app/About/page.js
+++ app/About/page.js
@@ -10,7 +10,7 @@ const About = () => {
         <br /><br />
         With a background in various roles including sales at IKEA and H&M, I have developed strong interpersonal skills and a deep understanding of customer needs. My journey in the world of technology started as a self-driven learner and brought me to Changemaker Educations, where I am honing my skills as a Frontend and App Developer.
         <br /><br />
-        Through my experience at IKEA as a Sales Associate since September 2019, and at H&M as a Salesperson from July 2022 to December 2022, I've gained valuable insights into customer behavior and effective communication.
+        Through my experience at IKEA as a Sales Associate since September 2019, and at H&M as a Salesperson from July 2022 to December 2022, I have gained valuable insights into customer behavior and effective communication.
         <br /><br />
         Currently, I am pursuing opportunities to apply my skills and contribute to innovative projects in the realm of web development. I am enthusiastic about building intuitive and delightful digital solutions that make a difference.
         <br /><br />
